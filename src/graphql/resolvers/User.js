06ff6e514354e5d96813c9c7e8aa05c1ed645319
DeterminedSlav <slavimitev10@gmail.com>
2020-04-12 10:06:39 +0300

    updates

--- src/graphql/resolvers/User.js
+++ src/graphql/resolvers/User.js
@@ -34,12 +34,19 @@ export default {
                 })
             })
         },
-        editUser: (root, {_id, username, email, password}) => {
+        editUser: async (root, {_id, username, email, password}) => {
+            const response = await User.findByIdAndUpdate({_id}, {$set: {username, email, password}}, {new: true}).exec();
+            if(!response){
+                throw new Error(`Cannot save user: ${_id}`);
+            }
+            return response;
+        }
+        /*editUser: (root, {_id, username, email, password}) => {
             return new Promise((resolve, reject) => {
                 User.findByIdAndUpdate({_id}, {$set: {username, email, password}}, {new: true}).exec((error, response) => {
                     error ? reject(error) : resolve(response);
                 })
             })
-        }
+        } */
     }
 }
